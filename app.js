beaa0bb5cb4eeb1c152155030aab3bce738197c1
singhsduos <ns9628491678@gmail.com>
2021-11-13 23:39:33 +0530

    API fetched

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const username = document.getElementById('name');
-const mail = document.getElementById('mail');
-const phone = document.getElementById('phone');
-const submitBtn = document.getElementById('submitBtn');
-const errorText = document.getElementById('errorMsg');
+// All Elements from HTML Doc
+const username = document.getElementById('name'),
+    mail = document.getElementById('mail'),
+    phone = document.getElementById('phone'),
+    submitBtn = document.getElementById('submitBtn'),
+    errorText = document.getElementById('errorMsg'),
+    country = document.getElementById('country'),
+    stateDiv = document.getElementById('stateDiv');
+let state = "";
 
+// Time Interval for reading state value for every new country selected
+setInterval(stateFunc, 100);
 
+function stateFunc() {
+    state = document.getElementById('state');
+}
+
+// validation for name
 function nameLength() {
     const usernameLength = username.value.length;
     const error = username.value;
@@ -14,6 +25,7 @@ function nameLength() {
     }
 }
 
+// validation for phone
 function phoneLength() {
     const phoneNumLength = phone.value.length;
     const error = phone.value;
@@ -23,6 +35,7 @@ function phoneLength() {
     }
 }
 
+// validation for  mail
 function validEmail() {
     const error = mail.value;
     const errReason = "should only support valid email address."
@@ -35,19 +48,97 @@ function validEmail() {
     }
 }
 
+// validation for country
+function valiCountry() {
+    const error = "Country";
+    const errReason = "is mandatory fields"
+    if (country.value == "") {
+        errorShow(error, errReason);
+    }
+}
+
+// validation for state
+function validState() {
+    const errReason = "is mandatory fields"
+    if (state.value == "") {
+        errorText.innerText = `Result: {"State": {"error": "${errReason}"}}`
+    }
+}
+
+
+// function to show error to user
+function errorShow(errorPlace, errorReason) {
+    errorText.innerText = `Result: {"${errorPlace}": {"error": "${errorReason}"}}`
+}
+
+// function to show success, when all fields are valid
 function allValidField() {
     errorText.innerText = `Result: {"Success": "All fields are valid."}}`
 }
 
-
+//  event listener for submit button
 submitBtn.addEventListener('click', function (event) {
     event.preventDefault();
     allValidField();
     nameLength();
     validEmail();
     phoneLength();
+    validState();
+    valiCountry();
 });
 
-function errorShow(errorPlace, errorReason) {
-    errorText.innerText = `Result: {"${errorPlace}": {"error":" "${errorReason}"}}`
+// API Fetching for countries and there state
+fetch('https://raw.githubusercontent.com/stefanbinder/countries-states/master/countries.json')
+    .then(response => response.json())
+    .then(country_list => {
+        let i = 0;
+        for (; i < country_list.length; i++) {
+            const options = document.createElement("option");
+            options.innerText = country_list[i].name;
+            options.setAttribute("value", `${country_list[i].code3}`)
+            country.appendChild(options);
+
+        }
+
+    })
+    .catch(error => console.error(error));
+
+
+function selectCountry() {
+
+    stateDiv.removeChild(stateDiv.lastChild);
+
+    fetch('https://raw.githubusercontent.com/stefanbinder/countries-states/master/countries.json')
+        .then(response => response.json())
+        .then(country_list => {
+            let i = 0;
+            let y = 0;
+
+            let select = `<label for="state">State</label><br>
+                          <select class="formInput" id="state">
+                              <option value='' selected="selected"> -- Select Country First -- </option>
+                          </select>`
+
+            stateDiv.innerHTML = select;
+
+            for (; i < country_list.length; i++) {
+
+                if (country.value == country_list[i].code3) {
+
+                    const statesList = country_list[i].states;
+
+                    for (; y < statesList.length; y++) {
+                        let options = document.createElement("option");
+                        options.innerText = statesList[y].name;
+                        options.setAttribute("value", `${statesList[y].code}`)
+                        stateDiv.lastChild.appendChild(options);
+                    }
+
+                }
+
+
+            }
+
+        })
+        .catch(error => console.error(error));
 }
